d2292e16cbf538ea5d4b398c0c83ca2c54028073
marcus6183 <marcusw6183@gmail.com>
2024-02-13 23:21:17 +0800

    Add ScrollToTop util
    Added to main.jsx before the BrowserRouter component so it will always scroll to top when the user navigates between the pages
    
    Note: main.jsx - Implemented Provider component for working with Redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
+import ScrollToTop from "./utils/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ScrollToTop />
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
